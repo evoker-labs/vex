fffe2168a7a1b63deb97856f7d55bc43bb537b6d
c0utin <rafaelcouto111@gmail.com>
2025-05-20 11:19:48 -0300

    [server] add stable memory

--- vex/src/vex_backend/src/lib.rs
+++ vex/src/vex_backend/src/lib.rs
@@ -1,4 +1,33 @@
 #[ic_cdk::query]
-fn greet(name: String) -> String {
+fn heart_beat(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
+use std::cell::RefCell;
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static MAP: RefCell<StableBTreeMap<u128, u128, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
+        )
+    );
+}
+
+#[ic_cdk::query]
+fn get(key: u128) -> Option<u128> {
+    MAP.with(|p| p.borrow().get(&key))
+}
+
+#[ic_cdk::update]
+fn insert(key: u128, value: u128) -> Option<u128> {
+    MAP.with(|p| p.borrow_mut().insert(key, value))
+}
+
+ic_cdk::export_candid!();
